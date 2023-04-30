5bc08b804cb5d8cab33298acefc14f12ed48f41c
shafiralina <shafira.lina@bee.id>
2023-04-30 22:58:55 +0700

    + auto migrate

--- models/base.go
+++ models/base.go
@@ -10,10 +10,11 @@ import (
 var db *gorm.DB
 
 func init() {
-	//os.Setenv("db_user", "root")
-	//os.Setenv("db_name", "skyshi")
-	//os.Setenv("db_host", "localhost")
-	//os.Setenv("db_pass", "shafira")
+	//os.Setenv("MYSQL_USER", "root")
+	//os.Setenv("MYSQL_DBNAME", "skyshi")
+	//os.Setenv("MYSQL_HOST", "localhost")
+	//os.Setenv("MYSQL_PASSWORD", "shafira")
+	//os.Setenv("MYSQL_PORT", "3306")
 
 	username := os.Getenv("MYSQL_USER")
 	password := os.Getenv("MYSQL_PASSWORD")
@@ -27,6 +28,7 @@ func init() {
 	if err != nil {
 		fmt.Print(err)
 	}
+	db.AutoMigrate(&Activities{}, &Todos{})
 
 	db = conn
 }
